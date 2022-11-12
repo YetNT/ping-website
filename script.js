8b5038eedb0e82cd5b13f7a9b4e18be1458d1e99
Flo Ro Ro <111688520+FloOnDaCode@users.noreply.github.com>
2022-11-12 08:26:18 +0000

    JS improvements and the about page

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
+var http = require('http')
 const input = document.getElementById("input").value
 const output = document.getElementById("output");
 const button = document.getElementById("lol")
 
 button.addEventListener("click", myFunction);
-  
-function myFunction() {
-    $.ajax({
-        url: input,
-        method: 'HEAD',
-        start_time: new Date().getTime(),
-        success: function(data) {
-            output.innerHTML = 'This request took ' + (new Date().getTime() - this.start_time) / 1000 +' seconds ' + '(Ping will be here)' 
-        }
-    });
 
+
+var options = {
+  host: input,
+  port: 80,
+  path: '/index.html'
+};
+
+http.get(options, function(res) {
+  if (res.statusCode == 200) {
+    console.log("success");
+  }
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
+function myFunction() {
+  $.ajax({
+    url: input,
+    method: 'HEAD',
+    start_time: new Date().getTime(),
+    success: function(data) {
+      output.innerHTML = 'Ping = ' + (new Date().getTime() - this.start_time) + 'ms'
+    }
+  });
 }
