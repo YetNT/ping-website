2d87b1648f774e49bd0a66c01f5d31d6aed92047
Yet <78223322+Yetity@users.noreply.github.com>
2022-11-12 07:30:23 +0000

    code wokrs now

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const output = document.getElementById("output");
 const button = document.getElementById("lol")
 
 button.addEventListener("click", myFunction);
-
+  
 function myFunction() {
-  $.ajax({
-    url: input,
-    success: function(result){
-      alert('reply');
-    },    
-      error: function(result){
-        alert('timeout/error');
-      }
-  });
-};
+    $.ajax({
+        url: input,
+        method: 'HEAD',
+        start_time: new Date().getTime(),
+        success: function(data) {
+            output.innerHTML = 'This request took ' + (new Date().getTime() - this.start_time) / 1000 +' seconds ' + '(Ping will be here)' 
+        }
+    });
+
+}
