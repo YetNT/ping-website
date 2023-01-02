3a3c009f2d6430b2804a8e16bf336cc2d2e3238e
Yet <78223322+Yetity@users.noreply.github.com>
2023-01-02 17:08:59 +0000

    Finnaly Works perfec now

---
+++ script.js
@@ -0,0 +1,18 @@
+
+async function ping(url) {
+  const startTime = performance.now();
+  try {
+    await fetch(url, { mode: "no-cors" });
+    const endTime = performance.now();
+    return `Response time: ${endTime - startTime}ms`;
+  } catch (error) {
+    return `Error: ${error}`;
+  }
+}
+
+console.log(ping("https://google.com"))
+
+async function myFunction() {
+  const output = await ping(document.getElementById("input").value);
+  document.getElementById("output").innerHTML = output;
+}
